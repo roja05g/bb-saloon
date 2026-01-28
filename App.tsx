41ce73d7012dd06f953cda50a591915f119c22a8
goddumarri roja <goddumarri.roja@testyantra.in>
2026-01-28 15:19:23 +0530

    map

--- App.tsx
+++ App.tsx
@@ -341,17 +341,15 @@ const App: React.FC = () => {
         </div>
 
         <div className="h-[500px] bg-stone-200 rounded-3xl overflow-hidden relative">
-           <img 
-            src="https://picsum.photos/800/800?random=50" 
-            alt="Map location placeholder" 
-            className="w-full h-full object-cover grayscale opacity-60"
-          />
-          <div className="absolute inset-0 flex items-center justify-center">
-            <div className="bg-white p-4 rounded-full shadow-xl">
-               <MapPin className="w-8 h-8 text-stone-900" />
-            </div>
-          </div>
-        </div>
+  <iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.027217886191!2d77.55557547412134!3d13.033938713510414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae171a92440d35%3A0x572d7b52eb334718!2sBlue%20Saloon!5e0!3m2!1sen!2sin!4v1769593035331!5m2!1sen!2sin"
+    className="w-full h-full border-0"
+    allowFullScreen
+    loading="lazy"
+    referrerPolicy="no-referrer-when-downgrade"
+  />
+</div>
+
       </div>
     </div>
   );
